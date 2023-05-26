b951a058f20ce990430684e13ef566a88bc28c12
SimoneVilardo <simovila95@gmail.com>
2023-05-26 17:05:52 +0200

    BONUS TERMINATO

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -3,33 +3,80 @@ function creazioneQuadrato(){
     // CREO UNA VARIABILE CHE CREI IL DIV
     let quadrato = document.createElement('div');
 
-    // AGGIUNGO UNA CLASSE 
-    quadrato.classList.add('quadrato');
     return quadrato;
 };
 
 // RECUPERO DAL DOM IL BOTTONE
 const bottone = document.getElementById('btn');
 
+// RECUPERO LA SELECT DAL DOM
+let selezione = document.getElementById('difficolta');
+
 // RECUPERO DAL DOM L'ELEMENTO IN CUI DEVO CREARE LA GRIGLIA
 const griglia = document.getElementById('griglia');
 
 //FACCIO IN MODO CHE CON IN CLICK APPAIA LA GRIGLIA
 bottone.addEventListener('click', function(){
-    for( let i = 0; i < 100; i++){
-        //CREO IL SINGOLO QUADRATO
-        let quadrato = creazioneQuadrato();
-        quadrato.innerText = i + 1;
-        
-        // AL CLICK NEL QUADRATO ESSO CAMBIA COLORE E RESTITUISCE IN CONSOLE IL NUEMRO DELLA CASELLA
-        quadrato.addEventListener('click', function(){
-            this.classList.toggle('cambio-colore');
-            console.log(quadrato.innerText)
-        });
-        
-        // MOSTRO TUTTO A SCHERMO CON .APPEND()
-        griglia.append(quadrato);
-        bottone.classList.add('d-none')
-    };
+    griglia.innerHTML = '';
+    let difficolta = selezione.value;
+    // CONDIZIONE PER FAR APPARIRE LE VARIE GRIGLIE IN BASE ALLA DIFFICOLTA SCELTA
+    if(difficolta === 'Facile'){
+   
+        for( let i = 0; i < 49; i++){
+            //CREO IL SINGOLO QUADRATO
+            let quadrato = creazioneQuadrato();
+            // AGGIUNGO UNA CLASSE 
+            quadrato.classList.add('quadrato-tre');
+    
+            quadrato.innerText = i + 1;
+            
+            // AL CLICK NEL QUADRATO ESSO CAMBIA COLORE E RESTITUISCE IN CONSOLE IL NUEMRO DELLA CASELLA
+            quadrato.addEventListener('click', function(){
+                this.classList.toggle('cambio-colore');
+                console.log(quadrato.innerText)
+            });
+            
+            // MOSTRO TUTTO A SCHERMO CON .APPEND()
+            griglia.append(quadrato);
+        };
 
+    } else if (difficolta === 'Normale'){
+
+        for( let i = 0; i < 81; i++){
+            //CREO IL SINGOLO QUADRATO
+            let quadrato = creazioneQuadrato();
+            // AGGIUNGO UNA CLASSE 
+            quadrato.classList.add('quadrato-due');
+    
+            quadrato.innerText = i + 1;
+            
+            // AL CLICK NEL QUADRATO ESSO CAMBIA COLORE E RESTITUISCE IN CONSOLE IL NUEMRO DELLA CASELLA
+            quadrato.addEventListener('click', function(){
+                this.classList.toggle('cambio-colore');
+                console.log(quadrato.innerText)
+            });
+            
+            // MOSTRO TUTTO A SCHERMO CON .APPEND()
+            griglia.append(quadrato);
+        };
+
+    } else {
+        for( let i = 0; i < 100; i++){
+            //CREO IL SINGOLO QUADRATO
+            let quadrato = creazioneQuadrato();
+            // AGGIUNGO UNA CLASSE 
+            quadrato.classList.add('quadrato');
+    
+            quadrato.innerText = i + 1;
+            
+            // AL CLICK NEL QUADRATO ESSO CAMBIA COLORE E RESTITUISCE IN CONSOLE IL NUEMRO DELLA CASELLA
+            quadrato.addEventListener('click', function(){
+                this.classList.toggle('cambio-colore');
+                console.log(quadrato.innerText)
+            });
+            
+            // MOSTRO TUTTO A SCHERMO CON .APPEND()
+            griglia.append(quadrato);
+        };
+    }
 })
--- BONUS/readme.md
+++ BONUS/readme.md
@@ -2,3 +2,11 @@ PROBLEMA:Aggiungere una select accanto al bottone di generazione, che fornisca u
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+SOLUZIONE:
+
+1.creo all'interno del click del bottone un if 
+    1.1 recupero la select
+    1.2 SE seleziono la difficoltà facile e clicco play inserirà la griglia con 49 quadrati
+    1.3 ALTRIMENTI SE seleziono la difficoltà normale e clicco play inserirà la griglia con 81 quadrati
+    1.4 ALTRIMENTI seleziono la difficoltà difficile e clicco play inserirà la griglia con 100 quadrati
